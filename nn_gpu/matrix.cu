006dd34094fef4aa0c576ee28ee53baa9de9a1bf
Aman Gupta <amang123@bu.edu>
2019-11-24 19:22:16 -0500

    matrix library

--- nn_gpu/matrix.cu
+++ nn_gpu/matrix.cu
@@ -8,8 +8,8 @@
 /******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "matrix.cuh"
-
 /******************************************************************************/
 /* Implementations */
 /******************************************************************************/
@@ -43,4 +43,74 @@ matrix * matrix_init(int rows, int cols, int seed) {
         m -> data[i] = (float)rand();
 
     return m;
+}
+
+/* Matrix initialization on Host
+Input:  Matrix
+Output: 0 on successfull deletion of matrix, -1 otherwise
+*/
+__host__
+int matrix_delete(matrix *mat) {
+    if (!mat) return -1;
+    assert(mat -> data);
+    free(mat -> data);
+    free(mat);
+    return 0;
+}
+
+/* Matrix Multiplication on host
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes global memory view
+*/
+__host__
+int MMM(matrix *A, matrix *B, matrix *out) {
+    if ((!A) || (!B) || (!out)) return -1;
+    if ((A->cols != B->rows) ||
+        (A->rows != out->rows) ||
+        (B->cols != out->cols)) return -2;
+    
+    int row, col, k;
+    for (col = 1; col <= B->cols; col++)
+        for (row = 1; row <= A->rows; row++) {
+            data_t val = (data_t)0;
+            for (k = 1; k <= A->cols; k++)
+                val += ELEMENT(A, row, k) * ELEMENT(B, k, col);
+            ELEMENT(prod, row, col) = val;
+        }
+    return 0;
+}
+
+/* Matrix Multiplication kernel with global memory
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes global memory view
+*/
+__global__
+void MMM_global(matrix *A, matrix *B, matrix *out) {
+
+}
+
+/* Matrix Multiplication kernel with shared memory
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes shared memory view
+*/
+__global__
+void MMM_shared(matrix *A, matrix *B, matrix *out) {
+
+}
+
+/* Matrix Multiplication kernel with unified memory
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes unified memory view
+*/
+__global__
+void MMM_unified(matrix *A, matrix *B, matrix *out) {
+    
 }
--- nn_gpu/matrix.cuh
+++ nn_gpu/matrix.cuh
@@ -28,3 +28,49 @@ Output: NULL in case the rows or column are not positive, or failure in memory
 */
 __host__
 matrix * matrix_init(int rows, int cols, int seed);
+
+/* Matrix initialization on Host
+Input:  Matrix
+Output: 0 on successfull deletion of matrix, -1 otherwise
+*/
+__host__
+int matrix_delete(matrix *mat);
+
+/* Accessing matrix element */
+#define ELEMENT(mat, row, col) mat->data[(col-1)*(mat->rows) + (row-1)]
+
+/* Matrix Multiplication on host
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes global memory view
+*/
+__host__
+int MMM(matrix *A, matrix *B, matrix *out);
+
+/* Matrix Multiplication kernel with global memory
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes global memory view
+*/
+__global__
+int MMM_global(matrix *A, matrix *B, matrix *out);
+
+/* Matrix Multiplication kernel with shared memory
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes shared memory view
+*/
+__global__
+int MMM_shared(matrix *A, matrix *B, matrix *out);
+
+/* Matrix Multiplication kernel with unified memory
+Input: Matrix A, B, out
+Output: A*B stored in out and 0 on success. returns -1 on failure
+        Assumes inner dimensions match, returns -2 if they dont match
+        Assumes unified memory view
+*/
+__global__
+int MMM_unified(matrix *A, matrix *B, matrix *out);
