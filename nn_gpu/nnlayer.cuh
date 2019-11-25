15bbd18055b188987254162dcfb21cde0895b44a
Aman Gupta <amang123@bu.edu>
2019-11-24 22:24:56 -0500

    working layer

--- nn_gpu/nnlayer.cuh
+++ nn_gpu/nnlayer.cuh
@@ -6,37 +6,139 @@
 /******************************************************************************/
 /* Libraries */
 #include "matrix.cuh"
-
+#include "activation.cuh"
 /******************************************************************************/
 /* prototypes and usage */
 /******************************************************************************/
+typedef struct NNLayer {
+    matrix *Z;  // output matrix
+    matrix *W;  // weights of current layer (Weight weights)
+    matrix *A;  // activation of previous layer (Activation Matrix)
+    matrix *b;  // bias vector
+} nnlayer;
 
-/* Single GPU prototypes */
+/* Feed Forward NN Forward pass (FFNNFP) on a layer */
 /******************************************************************************/
 
 /* Forward Propagation using global memory
-Input:  pointer to output data matrix, pointers to input matrix A and B,
-        size of matrix A and width of Matrix B, and current GPUid
-        Assumes width of A matches height of B; and global memory view
-Output: multiplication output saved in location pointed by output data matrix
+Input:  pointer to output data matrix Z, pointers to Weight matrix W,
+        and activation matrix A, bias vector b; x,y dimensions of W and A.
+        Assumes global memory view
+Output: Z = WA+b, output saved in location pointed by output data matrix
 */
 __global__
-void FFNNFP_global(data_t *out, data_t *A, data_t *B, int Ax, int Ay, int By);
+void FFNNFP_global(data_t *Z, data_t *W, data_t *A, data_t *b, int Wx, int Wy,
+    int Ax, int Ay);
 
 /* Forward Propagation using shared memory
-Input:  pointer to output data matrix, pointers to input matrix A and B,
-        size of matrix A and width of Matrix B, and current GPUid
-        Assumes width of A matches height of B; and shared memory view
-Output: multiplication output saved in location pointed by output data matrix
+Input:  pointer to output data matrix Z, pointers to Weight matrix W,
+        and activation matrix A, bias vector b; x,y dimensions of W and A.
+        Assumes shared memory view
+Output: Z = WA+b, output saved in location pointed by output data matrix
 */
 __global__
-void FFNNFP_shared(data_t *out, data_t *A, data_t *B, int Ax, int Ay, int By);
+void FFNNFP_shared(data_t *Z, data_t *W, data_t *A, data_t *b, int Wx, int Wy,
+    int Ax, int Ay);
 
-/* Forward Propagation using Unified memory (from CUDA 6 onwards)
-Input:  pointer to output data matrix, pointers to input matrix A and B,
-        size of matrix A and width of Matrix B, and current GPUid
-        Assumes width of A matches height of B; and unified memory view
-Output: multiplication output saved in location pointed by output data matrix
+/* Forward Propagation using unified memory
+Input:  pointer to output data matrix Z, pointers to Weight matrix W,
+        and activation matrix A, bias vector b; x,y dimensions of W and A.
+        Assumes unified memory view
+Output: Z = WA+b, output saved in location pointed by output data matrix
 */
 __global__
-void FFNNFP_unified(data_t *out, data_t *A, data_t *B, int Ax, int Ay, int By);
+void FFNNFP_unified(data_t *Z, data_t *W, data_t *A, data_t *b, int Wx, int Wy,
+    int Ax, int Ay);
+
+/* Feed Forward NN Back Prop (FFNNBP) on a layer */
+/******************************************************************************/
+/* Back Propagation using global memory
+Input:  pointer to output data matrix dA, pointers to Weight matrix W,
+        and backprop out matrix dZ; x,y dimensions of W and dZ.
+        Assumes global memory view
+Output: dA = W'.dZ, output saved in location pointed by output data matrix
+*/
+__global__
+void FFNNBP_global(data_t *dA, data_t *W, data_t *dZ, int Wx, int Wy,
+    int dZx, int dZy);
+
+/* Back Propagation using shared memory
+Input:  pointer to output data matrix dA, pointers to Weight matrix W,
+        and backprop out matrix dZ; x,y dimensions of W and dZ.
+        Assumes shared memory view
+Output: dA = W'.dZ, output saved in location pointed by output data matrix
+*/
+__global__
+void FFNNBP_shared(data_t *dA, data_t *W, data_t *dZ, int Wx, int Wy,
+    int dZx, int dZy);
+
+/* Back Propagation using unified memory
+Input:  pointer to output data matrix dA, pointers to Weight matrix W,
+        and backprop out matrix dZ; x,y dimensions of W and dZ.
+        Assumes unified memory view
+Output: dA = W'.dZ, output saved in location pointed by output data matrix
+*/
+__global__
+void FFNNBP_unified(data_t *dA, data_t *W, data_t *dZ, int Wx, int Wy,
+    int dZx, int dZy);
+
+/******************************************************************************/
+/*************************** GRADIENT DESCENT *********************************/
+/******************************************************************************/
+/* Feed Forward NN Update weights (FFNNUW) of a layer */
+/******************************************************************************/
+/* Input: pointer to updated weight matrix W, backprop out matrix dZ,
+          Activation matrix A; x,y dimensions of dZ and A;
+          learning rate (lr)
+          Assumes global memory view
+Output: W_ = W - lr*dW; dW = (1/numData)*(dZ.A'), updates saved in W
+*/
+__global__
+void FFNNUW_global(data_t *W, data_t *dZ, data_t *A, int dZx, int dZy, int Ax,
+    int Ay, data_t lr);
+
+/* Input: pointer to updated weight matrix W, backprop out matrix dZ,
+          Activation matrix A; x,y dimensions of dZ and A;
+          learning rate (lr)
+          Assumes shared memory view
+Output: W_ = W - lr*dW; dW = (1/numData)*(dZ.A'), updates saved in W
+*/
+__global__
+void FFNNUW_shared(data_t *W, data_t *dZ, data_t *A, int dZx, int dZy, int Ax,
+    int Ay, data_t lr);
+
+/* Input: pointer to updated weight matrix W, backprop out matrix dZ,
+          Activation matrix A; x,y dimensions of dZ and A;
+          learning rate (lr)
+          Assumes unified memory view
+Output: W_ = W - lr*dW; dW = (1/numData)*(dZ.A'), updates saved in W
+*/
+__global__
+void FFNNUW_unified(data_t *W, data_t *dZ, data_t *A, int dZx, int dZy, int Ax,
+    int Ay, data_t lr);
+
+/* Feed Forward NN Update bias (FFNNUb) of a layer */
+/******************************************************************************/
+/* Input: pointer to updated bias vector b, backprop out matrix dZ;
+          x,y dimensions of dZ and dim of b; learning rate (lr)
+          Assumes global memory view
+Output: b_ = b - lr*db; db = (1/numData)*Sum_{i=0...m}(dZi), updates saved in W
+*/
+__global__
+void FFNNUb_global(data_t *b, data_t *dZ, int dZx, int dZy, int bx, data_t lr);
+
+/* Input: pointer to updated bias vector b, backprop out matrix dZ;
+          x,y dimensions of dZ and dim of b; learning rate (lr)
+          Assumes shared memory view
+Output: b_ = b - lr*db; db = (1/numData)*Sum_{i=0...m}(dZi), updates saved in W
+*/
+__global__
+void FFNNUb_shared(data_t *b, data_t *dZ, int dZx, int dZy, int bx, data_t lr);
+
+/* Input: pointer to updated bias vector b, backprop out matrix dZ;
+          x,y dimensions of dZ and dim of b; learning rate (lr)
+          Assumes unified memory view
+Output: b_ = b - lr*db; db = (1/numData)*Sum_{i=0...m}(dZi), updates saved in W
+*/
+__global__
+void FFNNUb_unified(data_t *b, data_t *dZ, int dZx, int dZy, int bx, data_t lr);
